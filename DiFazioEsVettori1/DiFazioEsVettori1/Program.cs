c81410be8e1d55155a31754ccbbc2d054e4c69b9
Matteo Di Fazio <matteodifazio05@gmail.com>
2022-03-11 10:20:45 +0100

    Esercizio1Vettori

---
+++ DiFazioEsVettori1/DiFazioEsVettori1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DiFazioEsVettori1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 400;
+            int i = 0;
+            Random generatore = new Random();
+            for (int c = 0; c < dimensione; c++)
+            {
+                int numerocasuale = generatore.Next(0, 101);
+                Console.WriteLine($"{numerocasuale}");
+            }
+
+
+        }
+    }
+}
